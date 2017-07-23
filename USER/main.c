8a7a2f2b033438cfdce8280a5e577e4de31fb1d9
y <xuyaolalala@163.com>
2017-07-23 09:31:27 +0800

    添加小车直行程序

--- USER/main.c
+++ USER/main.c
@@ -7,10 +7,13 @@
 #include "encoder.h"
 #include "PID.h"
 #include "servo.h"
+#define PI 3.1415936
+#define DIAMETER 62 //车轮直径62mm
 //#define INF 3.402823466e+38F
 u8 pct1=70;
 u8 pct2=70;
 u8 i,len;
+u16 dis;//走过距离
 u16 inputEncoderVal=0;
 extern int leftEncoderVal;
 extern int rightEncoderVal;
@@ -26,22 +29,42 @@ void setup()
 	uart_init(115200);//串口初始化必须放在电机初始化后
 	Encoder1_Init_TIM4();
 	Encoder2_Init_TIM3();
-	TIM2_Int_Init(499, 8399); //84Mhz/8400
-	servoPwmInit(99,2766);//300hz 84Mhz
+	TIM2_Int_Init(499, 8399); //0.05s
+	servoPwmInit(99,2766);
 	pidInit(0.01,pct1-1,pct1-99);//初始化刷新间隔 上下限
+}
+void backStraight(u8 pct1,u8 pct2)
+{
+	motor1SetPctback(pct1);
+	setDesiredPoint(leftEncoderVal);
+	pidOutput=refresh(rightEncoderVal);
+	if(leftEncoderVal<rightEncoderVal)
+	{	 motor2SetPctback(pct2-pidOutput);
+		 pct2=pct2-pidOutput;
+	}
+	else 
+	{	motor2SetPctback(pct2+pidOutput);
+		pct2=pct2+pidOutput;
+	}
 }
 int main(void)
 { 
 	setup();
     setServoDegree(43);//43时摇臂位于中间位置
 	delay_ms(1000);
-	motor1SetPct(pct1);//越大 转速越慢  70-38    30-130
-	motor2SetPct(pct2);
+	//motor1SetPct(pct1);//越大 转速越慢  70-33    30-116
+	//motor2SetPct(pct2);
 	//stopMotor();
-	setWeights(0.41, 0.01, 0.0001); 
-
+	setWeights(0.4, 0.01, 0.0001); 
     while(1) 
 	{ 
+	/*	dis=PI*DIAMETER*(1560*0.05/leftEncoderVal);
+		if(dis>1000)//80cm
+		{
+			stopMotor();
+		}*/ 	
+		backStraight(60,60);
+		
 		if(USART_RX_STA&0x8000)
 		{
 			len = USART_RX_STA & 0x3fff;
@@ -64,6 +87,7 @@ int main(void)
 			pct1=pct1-pidOutput;
 			printf("\r\n");
 		}
+		
 		printf("leftEncoderVal:%d\r\n",leftEncoderVal);
 		printf("rightEncoderVal:%d\r\n\r\n",rightEncoderVal);
 		ledToggle();
